4643a8bd8d4966ebc9dc5f219c45c0ff891bf871
thielen24 <thielen24@gmail.com>
2023-03-09 23:35:11 -0600

    adding first unit test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("The addition result is {}", add(2, 3));
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
